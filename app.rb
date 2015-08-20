2c5ab484937c47793108b54b7a1cc417b99ee126
alisa colon & Chris Underwood <student@epicodus.com>
2015-08-20 15:23:24 -0700

    the 4 basic commands work for the staff side of books.

--- app.rb
+++ app.rb
@@ -12,18 +12,43 @@ get("/") do
   erb(:index)
 end
 
-get("/public") do
-  erb(:public)
-end
-
-post("/public") do
-  erb(:public)
-end
+# get("/public") do
+#   erb(:public)
+# end
+#
+# post("/public") do
+#   erb(:public)
+# end
 
 get("/staff") do
+  @books = Book.all()
   erb(:staff)
+end
+
+get("/staff/book/:id") do
+  @book = Book.find(params.fetch("id").to_i())
+  erb(:book)
 end
 
 post("/staff") do
-  erb(:staff)
+  title = params.fetch("title")
+  author = params.fetch("author")
+  @books = Book.new({:title => title, :author=> author, :id => nil})
+  @books.save()
+  redirect('/staff')
+end
+
+patch('/staff/book/:id') do
+  title = params.fetch("title")
+  author = params.fetch("author")
+  @book = Book.find(params.fetch('id').to_i)
+  @book.update({:title => title, :author=> author})
+  redirect("/staff")
+end
+
+delete('/staff/book/:id') do
+  @book = Book.find(params.fetch('id').to_i)
+  @book.delete()
+  @books = Book.all()
+  erb(:success)
 end
--- spec/library_integration_spec.rb
+++ spec/library_integration_spec.rb
@@ -3,3 +3,13 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exception, false)
+
+describe('seeing all books and showing them on Sinatra', {:type => :feature}) do
+  it('allows a user to seeclick a list to see the tasks and details for it') do
+    test_book = Book.new({:title => "Harry Potter", :author => "JK Rowling", :id => nil})
+    test_book.save()
+    visit('/staff')
+    click_link(test_book.title())
+    expect(page).to have_content(test_book.title(), test_book.author())
+  end
+end
